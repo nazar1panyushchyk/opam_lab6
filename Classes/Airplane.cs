cd43642fc279d2489feb7655452ba207a9463e98
nazar1panyushchyk <npanusik@gmail.com>
2026-02-28 17:16:20 +0200

    add irefuelable in car and airplane with refill method

--- Classes/Airplane.cs
+++ Classes/Airplane.cs
@@ -1,13 +1,16 @@
 using System;
 
-class Airplane : Vehicle
+class Airplane : Vehicle, IRefuelable
 {
-    public Airplane(string brand, int speed) : base(brand, speed)
-    {
-    }
+    public Airplane(string brand, int speed) : base(brand, speed) { }
 
     public override void Move()
     {
         Console.WriteLine($"Літак {Brand} летить зі швидкістю {Speed} км/год.");
+    }
+
+    public void Refill()
+    {
+        Console.WriteLine($"Літак {Brand} заправляється авіаційним паливом.");
     }
 }
--- Classes/Car.cs
+++ Classes/Car.cs
@@ -1,13 +1,16 @@
 using System;
 
-class Car : Vehicle
+class Car : Vehicle, IRefuelable
 {
-    public Car(string brand, int speed) : base(brand, speed)
-    {
-    }
+    public Car(string brand, int speed) : base(brand, speed) { }
 
     public override void Move()
     {
         Console.WriteLine($"Автомобіль {Brand} їде зі швидкістю {Speed} км/год.");
+    }
+
+    public void Refill()
+    {
+        Console.WriteLine($"Автомобіль {Brand} заправляється паливом.");
     }
 }
